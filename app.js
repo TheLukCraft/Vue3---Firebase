9acc4d446701c9111ae626134f00973ca9240ff1
TheLukCraft <luk1997r@o2.pl>
2024-06-13 13:58:43 +0200

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.google.com",
       showBooks: true,
       books: [
-        { title: "name", author: "luk" },
-        { title: "name2", author: "luk2" },
-        { title: "name3", author: "luk3" },
+        { title: "name", author: "luk", img: "assets/book1.png" },
+        { title: "name2", author: "luk2", img: "assets/book2.png" },
+        { title: "name3", author: "luk3", img: "assets/book3.png" },
       ],
     };
   },
