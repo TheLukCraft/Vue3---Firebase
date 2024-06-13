4c21973498cb8c7ed47a8ed4c75388ec2bcc13fb
TheLukCraft <luk1997r@o2.pl>
2024-06-13 12:41:05 +0200

    other mouse events

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const app = Vue.createApp({
       title: "The Final Empire",
       author: "Luke Pat",
       age: "29",
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) console.log(data);
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
