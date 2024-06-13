a7b9b9362e8ce63ff8c7e9276fcfde8ff4bc9944
TheLukCraft <luk1997r@o2.pl>
2024-06-13 12:25:18 +0200

    conditional rendering

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
+      showBooks: true,
       title: "The Final Empire",
       author: "Luke Pat",
       age: "29",
     };
   },
   methods: {
-    changeTitle() {
-      this.title = "changed title";
-    },
-    changeTitleByArgument(title) {
-      this.title = title;
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
