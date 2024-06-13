ac5eda2f12073aed4eee92c59dc0c172e4e08c83
TheLukCraft <luk1997r@o2.pl>
2024-06-13 13:50:18 +0200

    outputting lists (v-for)

--- app.js
+++ app.js
@@ -2,24 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Luke Pat",
-      age: "29",
-      x: 0,
-      y: 0,
+      books: [
+        { title: "name", author: "luk" },
+        { title: "name2", author: "luk2" },
+        { title: "name3", author: "luk3" },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
-    },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) console.log(data);
-    },
-    handleMouseMove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
     },
   },
 });
