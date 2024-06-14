d4f8fade40cca0824bec8ba28a3ce227be523b72
TheLukCraft <luk1997r@o2.pl>
2024-06-14 09:26:59 +0200

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,23 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>Welcome...</p>
-  <teleport to="#modals" v-if="showModal">
-    <Modal @close="toggleModal">
-      <!-- name of slot is links -->
-      <template v-slot:links>
-        <a href="#">Sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Hi, Luke</h1>
-      <p>What's up?</p>
-    </Modal>
-  </teleport>
-
-  <!-- Second Modal -->
-
-  <teleport to="#modals" v-if="showModalTwo">
-    <Modal @close="toggleModalTwo">
-      <h1>Hi, Luke second time</h1>
-      <p>What's up brother?</p>
-    </Modal>
-  </teleport>
-  <button @click.alt="toggleModal">Open modal (alt)</button>
-  <button @click="toggleModalTwo">Open modal</button>
+  <h1>Ninja Reaction Timer</h1>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: { Modal },
-  data() {
-    return {
-      title: 'My First Vue App',
-      showModal: false,
-      showModalTwo: false,
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.showModal = !this.showModal;
-    },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo;
-    }
+  components: {
   }
 }
 </script>
 
 <style>
-#app,
-#modals {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
-}
-
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
 }
 </style>
