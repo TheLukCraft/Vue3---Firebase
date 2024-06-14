d5cbcadf992bab1dd984c3c2cee8aff53666766a
TheLukCraft <luk1997r@o2.pl>
2024-06-14 15:54:45 +0200

    submitting forms

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-    <form action="">
+    <form @submit.prevent="handleSubmit">
         <label for="">Email</label>
         <input type="email" required v-model="email">
 
         <label for="">Password</label>
         <input type="password" required v-model="password">
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
         <label>Role:</label>
         <select v-model="role">
@@ -22,6 +23,10 @@
         <div class="terms">
             <input type="checkbox" v-model="terms" required>
             <label>Accept terms and conditions</label>
+        </div>
+
+        <div class="submit">
+            <button>Create an Account</button>
         </div>
     </form>
     <p>Email: {{ email }}</p>
@@ -41,6 +46,7 @@ export default {
             terms: false,
             tempSkill: '',
             skills: [],
+            passwordError: '',
         }
     },
     methods: {
@@ -56,6 +62,19 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return skill !== item
             })
+        },
+        handleSubmit() {
+            //validate password
+            this.passwordError = this.password.length > 5 ?
+                '' : 'Password must be at least 6 chars long';
+            if (!this.passwordError) {
+                console.log('email: ', this.email)
+                console.log('password : ', this.password)
+                console.log('role: ', this.role)
+                console.log('skills: ', this.skills)
+                console.log('terms accepted: ', this.terms)
+
+            }
         }
     }
 }
@@ -111,5 +130,25 @@ input[type="checkbox"] {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+}
+
+button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+
+.submit {
+    text-align: center;
+}
+
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
