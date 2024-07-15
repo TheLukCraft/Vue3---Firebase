7a3ded0212552187e1a0455acb09446530cd116f
TheLukCraft <luk1997r@o2.pl>
2024-07-15 02:34:13 +0200

    bug fiexed

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,25 +1,26 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id">
-        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }"></router-link>
-        <h2>{{ job.title }}</h2>
+    <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+            <h2>{{ job.title }}</h2>
+        </router-link>
     </div>
 </template>
-
+  
 <script>
 export default {
     data() {
         return {
             jobs: [
-                { title: 'Luke UX Designer', id: 1, details: 'lorem' },
-                { title: 'Luke Web Designer', id: 2, details: 'lorem' },
-                { title: 'Luke Vue Designer', id: 3, details: 'lorem' }
-            ]
+                { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
+                { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
+                { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
+            ],
         }
     }
 }
 </script>
-
+  
 <style>
 .job h2 {
     background: #f4f4f4;
